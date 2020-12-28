08cff4c5fd4ac3e4c78bceff9100a9f372cb96ec
LEGEND <baralprem4@gmail.com>
2020-12-28 20:19:40 +0530

    code

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'kjijt96j88ud*sfmb+5dhfx2d!fgiwlqus1l+gpx#inp8s!+fm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-repo-app.herokuapp.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
@@ -121,8 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    BASE_DIR / "core/static",
+    BASE_DIR / "static",
 ]
